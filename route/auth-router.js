c491cd5788573dca412f0531af2f3ec23b8eed7c
noahgribbin <noahgribbin@yahoo.com>
2017-03-13 14:38:15 -0700

    auth POST signup route

---
+++ route/auth-router.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const debug = require('debug')('cornucopia:auth-router');
+const createError = require('http-errors');
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+
+const User = require('./model/user.js')
+const basicAuth = require('./lib/basic-auth-middleware.js');
+
+const authRouter = module.exports = Router();
+
+authRouter.post('/api/signup', jsonParser, function(req, res, next) {
+  // why json parser?
+  debug('POST /api/signup');
+
+  let password = req.body.password;
+  delete req.body.password;
+
+  let user = new User(req.body);
+  user.generatePasswordHash(password)
+  .then( user => user.save())
+  .then( user => user.generateToken())
+  .then( token => res.send(token))
+  .catch( err => next(createError(400, err.message)));
+});
