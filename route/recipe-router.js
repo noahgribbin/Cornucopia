2ee9b6c72ab6a9aa60dee7d5f2dbd366de86987a
0blu3 <braelynn.luedtke@gmail.com>
2017-03-13 14:47:28 -0700

    added put route and edited post route

--- route/recipe-router.js
+++ route/recipe-router.js
@@ -16,9 +16,17 @@ recipeRouter.post('/api/recipe', bearerAuth, jsonParser, function(req, res, next
 
   if (!req.body) return next(createError(400, 'expected body'));
   if (!req.user) return next(createError(400, 'expected user'));
-  req.body.profileID = req.user._id;
+  req.body.profileID = req.profile._id;
   return new Recipe(req.body).save()
-  .then(recipe => res.json(recipe))
+  .then(recipe => {
+    Profile.findById(recipe.profileID)
+    .then(profile => {
+      profile.recipes.push(recipe._id)
+      profile.save();
+    })
+    .catch(next);
+    res.json(recipe);
+  })
   .catch(next);
 });
 
@@ -34,7 +42,7 @@ recipeRouter.get('/api/recipe/:profileID', function(req, res, next) {
   debug('GET: /api/recipe/:profileID');
 
   Recipe.findById(req.params.profileID)
-  .then(profile.recipes => res.json(profile.recipes))
+  .then(profile => res.json(profile.recipes))
   .catch(next);
 });
 
@@ -48,12 +56,21 @@ recipeRouter.delete('/api/recipe/:id', bearerAuth, function(req, res, next) {
     if (recipeIndex === -1) return next(createError(404, 'not found'));
     recipeArray.splice(recipeIndex, 1);
     profile.recipes = recipeArray;
-    Profile.update( { req.user._id }, { $set: {recipes: recipeArray} } )
+    Profile.update( { _id: req.user._id }, { $set: {recipes: recipeArray} } )
     .then( () => {
       Recipe.findByIdAndRemove(req.params.id)
       res.status(204).send();
     })
     .catch(next);
   })
+  .catch(next);
+});
+
+recipeRouter.put('/api/recipe/:id', bearerAuth, jsonParser, function(req, res, next) {
+  debug('PUT: /api/recipe/:id');
+
+  if(req._body !== true) return next(createError(400, 'bad request'));
+  Recipe.findByIdAndUpdate(reqs.params.id, req.body, { new: true })
+  .then( recipe => res.json(recipe))
   .catch(next);
 });
