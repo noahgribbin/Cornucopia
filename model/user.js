fbb59451dca9588daad78b2a102faedf88cebcba
Khalid Mohamud <kmohamud1@outlook.com>
2017-03-13 13:33:37 -0700

    userSchema and password methods

---
+++ model/user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+const Promise = require('bluebird');
+const debug = require('debug')('cornucopia:user');
+
+const Schema = mongoose.Schema;
+const userSchema = Schema({
+  username : {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  findHash: {type: String, required: true }
+});
+
+userSchema.methods.generatePasswordHash = function(password){
+  debug('generatePasswordHash');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) return reject(err);
+      this.password = hash;
+      resolve(this)
+    })
+  })
+}
+
+userSchema.methods.comparePasswordHash = function(password){
+  debug('comparePasswordHash');
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, this.password, (err, valid) => {
+      if(err) return reject(err);
+      if(!valid) return reject(createError(401, 'Wrong password!'));
+    });
+  });
+};
+
