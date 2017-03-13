9be4ca10111dec4d12ece38ef41f96ade37699c8
noahgribbin <noahgribbin@yahoo.com>
2017-03-13 14:58:40 -0700

    auth GETRauthRouter.get('/api/signin', basicAuth, function(req, res, next) {

--- route/auth-router.js
+++ route/auth-router.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const debug = require('debug')('cornucopia:auth-router');
-const createError = require('http-errors');
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
 
-const User = require('./model/user.js')
+const User = require('./model/user.js');
 const basicAuth = require('./lib/basic-auth-middleware.js');
 
 const authRouter = module.exports = Router();
@@ -21,6 +20,19 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next) {
   user.generatePasswordHash(password)
   .then( user => user.save())
   .then( user => user.generateToken())
+  // whats goin on with the token
   .then( token => res.send(token))
-  .catch( err => next(createError(400, err.message)));
+  .catch(next);
+});
+
+authRouter.get('/api/signin', basicAuth, function(req, res, next) {
+  // why not json parser?
+  debug('GET /api/signin');
+
+  User.findOne({ username: req.auth.username})
+  .then( user => user.comparePasswordHash(req.auth.password))
+  .then( user => user.generateToken())
+  // whats goin on with the token
+  .then( token => res.send(token))
+  .catch(next);
 });
