adf1f40e9a7f688fcfa4b89cd8d08f5ded92eb75
radenska <yana_radenska@yahoo.com>
2017-03-13 13:35:55 -0700

    added post route

---
+++ route/recipe-router.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const debug = require('debug')('cornucopia:recipe-router');
+const fs = require('fs');
+const Promise = require('bluebird');
+const createError = require('http-errors');
+const jsonParser = require('body-parser');
+const Router = require('express').Router;
+const Recipe = require('../model/recipe.js');
+
+const recipeRouter = module.exports = Router();
+
+recipeRouter.post('/api/recipes', bearerAuth, jsonParser, function(req, res, next) {
+  debug('POST: /api/recipes');
+
+  if (!req.body) return next(createError(400, 'expected body'));
+  if (!req.user) return next(createError(400, 'expected user'));
+  req.body.profileID = req.user._id;
+  return new Recipe(req.body).save()
+  .then(recipe => res.json(recipe))
+  .catch(next);
+};
