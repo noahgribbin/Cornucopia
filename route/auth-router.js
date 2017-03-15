fd906a9add4e82e80ca5a1746716fd6dd3aa77d1
Khalid Mohamud <kmohamud1@outlook.com>
2017-03-15 10:25:57 -0700

    added delete and put routes for auth router

--- route/auth-router.js
+++ route/auth-router.js
@@ -37,3 +37,20 @@ authRouter.get('/api/signin', basicAuth, function(req, res, next) {
   .then( token => res.send(token))
   .catch(next);
 });
+
+authRouter.put('/api/account-settings', basicAuth, function(req, res, next) {
+  debug('PUT /api/signin');
+
+  User.findOne({ username : req.auth.username})
+  .then( user => user.comparePasswordHash(req.auth.password))
+  .then( user => User.findByIdAndUpdate(user._id, req.body, {new: true}))
+  .catch(next);
+});
+authRouter.delete('/api/account-settings', basicAuth, function(req, res, next) {
+  debug('DELETE /api/account-settings');
+
+  User.findOne({username: res.auth.username})
+  .then( user => User.findByIdAndRemove(user._id))
+  .then( () => res.status(204).send('Deleted successfuly'))
+  .catch(next);
+});
