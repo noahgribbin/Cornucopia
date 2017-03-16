3a889122815e80faba2c5c2330cdb4e8ee1484e0
radenska <yana_radenska@yahoo.com>
2017-03-16 14:12:52 -0700

    Update recipe-router-test.js

--- test/recipe-router-test.js
+++ test/recipe-router-test.js
@@ -281,7 +281,7 @@ describe('Recipe Routes', () => {
       .then( recipe => {
         this.tempRecipe = recipe;
         this.tempProfile.recipes.push(this.tempRecipe._id);
-        return Profile.findByIdAndUpdate(this.tempProfile._id, this.tempProfile, { new: true } );
+        return Profile.findByIdAndUpdate(this.tempProfile._id, { $set {recipes: this.tempProfile.recipes } }, { new: true } );
       })
       .then( () => done())
       .catch(done);
