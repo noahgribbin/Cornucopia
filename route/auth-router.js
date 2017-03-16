ec7a5a87384bac30387b803c611c1b98cbc11a1e
noahgribbin <noahgribbin@yahoo.com>
2017-03-16 16:16:39 -0700

    reverted master to how it was

--- route/auth-router.js
+++ route/auth-router.js
@@ -24,36 +24,13 @@ authRouter.post('/api/signup', jsonParser, function(req, res, next) {
   .catch( err => next(createError(400, err.message)));
 });
 
-authRouter.get('/', function(req, res, next) {
-  res.send('http://cdn.wonderfulengineering.com/wp-content/uploads/2014/03/high-resolution-wallpapers-7.jpg')
-})
-
 authRouter.get('/api/signin', basicAuth, function(req, res, next) {
   debug('GET /api/signin');
-
-  User.findOne( { username: req.auth.username} )
+  User.findOne({ username: req.auth.username})
   .then( user => {
     return user.comparePasswordHash(req.auth.password);
   })
   .then( user => user.generateToken())
   .then( token => res.send(token))
-  .catch(next);
-});
-
-authRouter.put('/api/account', basicAuth, jsonParser, function(req, res, next) {
-  debug('PUT /api/account');
-
-  if (!req._body) return next(createError(400, 'Expected request body'));
-  User.findOne( { username : req.auth.username} )
-  .then( user => user.comparePasswordHash(req.auth.password))
-  .then( user => {
-    if(req.body.password) {
-      user.generatePasswordHash(req.body.password)
-      .then( user => req.body.password = user._id)
-      .catch(next);    }
-    User.findByIdAndUpdate(user._id, req.body, {new: true} )
-    .then( user => res.json(user))
-    .catch(next);
-  })
   .catch(next);
 });
