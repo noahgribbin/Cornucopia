145599fded13a85f9ee0b1f48a1e26f518f10e91
radenska <yana_radenska@yahoo.com>
2017-03-16 17:08:42 -0700

    putting the PUT route back

--- route/auth-router.js
+++ route/auth-router.js
@@ -34,3 +34,21 @@ authRouter.get('/api/signin', basicAuth, function(req, res, next) {
   .then( token => res.send(token))
   .catch(next);
 });
+
+authRouter.put('/api/account', basicAuth, jsonParser, function(req, res, next) {
+  debug('PUT /api/account');
+
+  if (!req._body) return next(createError(400, 'Expected request body'));
+  User.findOne( { username : req.auth.username} )
+  .then( user => user.comparePasswordHash(req.auth.password))
+  .then( user => {
+    if(req.body.password) {
+      user.generatePasswordHash(req.body.password)
+      .then( user => req.body.password = user._id)
+      .catch(next);    }
+    User.findByIdAndUpdate(user._id, req.body, {new: true} )
+    .then( user => res.json(user))
+    .catch(next);
+  })
+  .catch(next);
+});
