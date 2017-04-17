2169889c3907aee55b2421f479e30ae4c45aadcc
radenska <yana_radenska@yahoo.com>
2017-04-17 09:38:53 -0700

    small tweak to get profile route

--- route/profile-router.js
+++ route/profile-router.js
@@ -23,10 +23,10 @@ profileRouter.post('/api/profile', bearerAuth, jsonParser, function(req, res, ne
   .catch(next);
 });
 
-profileRouter.get('/api/profile', bearerAuth, jsonParser, function(req, res, next) {
-  debug('GET: /api/profile');
+profileRouter.get('/api/profile/:id', function(req, res, next) {
+  debug('GET: /api/profile/:id');
 
-  Profile.findOne( {userID: req.user._id} )
+  Profile.findOne( {userID: req.params.id})
   .then( profile => res.json(profile))
   .catch(next);
 });
